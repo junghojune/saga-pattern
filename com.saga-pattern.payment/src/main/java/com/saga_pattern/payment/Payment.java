cef26498f77f3579b12f536483bddc72ae4e4c5b
hosu <hojune0904@gmail.com>
2024-08-16 13:24:02 +0900

    saga pattern payment 개발

---
+++ com.saga-pattern.payment/src/main/java/com/saga_pattern/payment/Payment.java
@@ -0,0 +1,1 @@
+package com.saga_pattern.payment;import lombok.Builder;import lombok.Data;import java.util.UUID;@Data@Builderpublic class Payment {    private UUID paymentId;    private String userId;    private String payAmount;    private String payStatus;}
